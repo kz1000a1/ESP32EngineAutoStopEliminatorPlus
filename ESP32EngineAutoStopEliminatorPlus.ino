3ac28c4b92e3bd5c9034557aa861ffd1935fe026
kz1000a1 <146339266+kz1000a1@users.noreply.github.com>
2024-05-10 08:38:40 +0900

    Update ESP32EngineAutoStopEliminatorPlus.ino

--- ESP32EngineAutoStopEliminatorPlus.ino
+++ ESP32EngineAutoStopEliminatorPlus.ino
@@ -178,12 +178,12 @@ void setup() {
   // Initialize configuration structures using macro initializers
   twai_general_config_t g_config = TWAI_GENERAL_CONFIG_DEFAULT((gpio_num_t)TX_PIN, (gpio_num_t)RX_PIN, TWAI_MODE_NORMAL);
   twai_timing_config_t t_config = TWAI_TIMING_CONFIG_500KBITS();  //Look in the api-reference for other speed sets.
-  // twai_filter_config_t f_config = TWAI_FILTER_CONFIG_ACCEPT_ALL();
-  twai_filter_config_t f_config = { .acceptance_code = CAN_ID_CCU << 21, .acceptance_mask = ((CAN_ID_CCU << 21) ^ (CAN_ID_TCU << 21)) | 0x1fffff, .single_filter = true };
+  twai_filter_config_t f_config = TWAI_FILTER_CONFIG_ACCEPT_ALL();
+  // twai_filter_config_t f_config = { .acceptance_code = CAN_ID_CCU << 21, .acceptance_mask = ((CAN_ID_CCU << 21) ^ (CAN_ID_TCU << 21)) | 0x1fffff, .single_filter = true };
 
-  if (DebugMode == CANDUMP) {
-    f_config = TWAI_FILTER_CONFIG_ACCEPT_ALL();
-  }
+  // if (DebugMode == CANDUMP) {
+  //   f_config = TWAI_FILTER_CONFIG_ACCEPT_ALL();
+  // }
 
   // Install TWAI driver
   if (twai_driver_install(&g_config, &t_config, &f_config) != ESP_OK) {
